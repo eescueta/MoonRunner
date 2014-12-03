ab4aa4e96c64ecdf4ebc2071d5a3d4e278018161
Nathan Tung <nathanctung@gmail.com>
2014-12-02 20:02:27 -0800

    Changed movement to texture scrolling, implemented basic navigation and speed adjustment

--- project.js
+++ project.js
@@ -15,6 +15,8 @@ var timer = new Timer();
 var x = 0; // x-axis displacement from origin (controls right/left)
 var y = 0; // y-axis displacement from origin (controls up/down)
 var z = 0; // z-axis displacement from origin (controls back/forward)
+var textureDegree = 0;
+var textureScrollSpeed = 0.005;
 
 // buffers for vertices and normals
 var positionBuffer;
@@ -62,15 +64,17 @@ window.onload = function init() {
 	document.onkeydown = function(e) {
 		e = e || window.event;
 		
-		// user can move camera foward or back using "i" and "o"
-		// furthermore, the user can move the camera left/right and up/down using arrow keys
-		
-		if(e.keyCode===73) { // "i" (move camera forward)
-			z+=0.05;
-			if(z>2) z=0;
+		if(e.keyCode===87) { // "w" (move forward)
+			z+=0.1;
 		}
-		else if(e.keyCode===79) { // "o" (move camera back)
-			z-=0.05;
+		else if(e.keyCode===83) { // "s" (move back)
+			z-=0.1;
+		}
+		else if(e.keyCode===65) { // "a" (move left)
+			x+=0.1;
+		}
+		else if(e.keyCode===68) { // "d" (move right)
+			x-=0.1;
 		}
 		else if(e.keyCode===38) { // "up" (move camera up)
 			y-=0.1;
@@ -78,17 +82,25 @@ window.onload = function init() {
 		else if( e.keyCode===40) { // "down" (move camera down)
 			y+=0.1;
 		}
-		else if(e.keyCode===37) { // "left" (move camera left)
-			x+=0.1;
+		else if(e.keyCode===37) { // "left" (turn left - not yet implemented)
+			textureDegree+=1;
 		}
-		else if(e.keyCode===39) { // "right" (move camera right)
-			x-=0.1;
+		else if(e.keyCode===39) { // "right" (turn right - not yet implemented)
+			textureDegree-=1;
+		}
+		else if(e.keyCode===73) { // "i" (speed up)
+			textureScrollSpeed+=0.0005;
+		}
+		else if(e.keyCode===79) { // "o" (slow down)
+			if(textureScrollSpeed>=0.0005)
+				textureScrollSpeed-=0.0005;
 		}
 		else if(e.keyCode===27) { // "esc" resets the camera to original position
 			x=0;
 			y=0;
 			z=0;
 		}
+		console.log(e.keyCode);
 	};
 	
 	// set up world, specifying the viewport, enabling depth buffer, and clearing color buffer
@@ -109,8 +121,8 @@ window.onload = function init() {
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image); // upload texture image to GPU
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST); // parameters for scaling up
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST); // parameters for scaling down
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); // prevent wrapped s coordinates (repeating)
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE); // prevent wrapped t coordinates
+		// gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); // prevent wrapped s coordinates (repeating)
+		// gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE); // prevent wrapped t coordinates
 		gl.bindTexture(gl.TEXTURE_2D, null);
     }
 	texture.image.src = "./Images/snow.jpg";
@@ -179,17 +191,12 @@ window.onload = function init() {
 	// reset timer and enable depth buffer before rendering
     timer.reset();	
     gl.enable(gl.DEPTH_TEST);
-	
+	    
     render();
 }
 
 function render() {
-	
-	// testing skid movement
-	// or alternatively scroll the texture!
-	z+=0.02;
-	if(z>2) z=0;
-	
+		
 	// clear buffers and update time based on timer
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     time += timer.getElapsedTime() / 1000;
@@ -205,6 +212,25 @@ function render() {
 	
 // slope
 
+    /*
+    // rotation of texture
+	// make a copy of uvArray
+	var uvArrayTemp = uvArray.slice();
+	// apply absolute rotational positioning to copy of uvArray
+	// absolute in the sense that the x and y components of uvArrayTemp are calculated anew each time, from 0 degrees to time*360 degrees
+	rotateUV(uvArrayTemp, textureDegree);		
+	// apply transformation via binding
+	gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(uvArrayTemp), gl.STATIC_DRAW);
+    */
+    
+    // scrolling of texture
+    // apply relative translational positioning to uvArray (x and y components are additively increased by those values on each render)
+	translateUV(uvArray, textureScrollSpeed, 0);
+	// apply transformation via binding
+	gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(uvArray), gl.STATIC_DRAW);
+    
 	// bind the normal texture coordinates
     gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
     gl.vertexAttribPointer(attribute_UV, 2, gl.FLOAT, false, 0, 0);
@@ -247,4 +273,43 @@ function slope(vertices, points, normals, uv) {
     points.push(vertices[0]);
     points.push(vertices[2]);
     points.push(vertices[3]);
+}
+
+//given a 2D matrix of rows comprising vec2 of texture coordinates, transform each vec2 to be rotated by theta
+function rotateUV(matrix, theta) {
+
+	var rad = theta*Math.PI/180;
+
+	for(var i=0; i<matrix.length; i++) {
+		var tempX = matrix[i][0];
+		var tempY = matrix[i][1];
+		
+		// texture rotates at an axis located at the corner of the cube
+		// we need to translate the texture coordinates there first (a diagonal of 0.5 units, as it's a unit cube)
+		tempX = tempX-0.5;
+		tempY = tempY-0.5;
+		
+		// apply the rotation
+		var newX = tempX*Math.cos(rad) + tempY*Math.sin(rad);
+		var newY = -tempX*Math.sin(rad) + tempY*Math.cos(rad);
+		
+		// then translate texture back to original position
+		newX = newX+0.5;
+		newY = newY+0.5;
+		
+		// make changes to the matrix
+		matrix[i] = [newX, newY];
+	}
+}
+
+// given a 2D matrix of rows comprising vec2 of texture coordinates, transform each vec2 to be translated by distance (separated by x and y components)
+function translateUV(matrix, distanceX, distanceY) {
+		for(var i=0; i<matrix.length; i++) {
+		// take x and y components of the vec2 and translate them
+		var newX = matrix[i][0]+distanceX;
+		var newY = matrix[i][1]+distanceY;
+		
+		// make changes to the matrix
+		matrix[i] = [newX, newY];
+	}
 }
